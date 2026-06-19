b6fddb2205ab7951ca73f968099a6f18db3667a4
JaviBaTap <ja.banares@duocuc.cl>
2026-06-19 16:08:51 -0400

    Add files via upload

---
+++ service-usuarios/service_usuarios/src/main/java/com/dreamysips/service_usuarios/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.dreamysips.service_usuarios.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI dreamySipsOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DreamySips - Usuarios API")
+                        .description("Microservicio de gestión de usuarios y autenticación")
+                        .version("1.0"));
+    }
+}
