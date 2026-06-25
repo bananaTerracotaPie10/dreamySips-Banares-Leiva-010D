21ad2c304eb22fc69205146f8608dd6e1d16bcc6
bananaTerracotaPie10 <f.leiva2@duocuc.cl>
2026-06-25 18:26:39 -0400

    dar acceso a Swagger UI y API docs

--- service-usuarios/service_usuarios/src/main/java/com/dreamysips/service_usuarios/config/SecurityConfig.java
+++ service-usuarios/service_usuarios/src/main/java/com/dreamysips/service_usuarios/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig
 
         return http.csrf(crsf -> crsf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
                 ).build();
 
